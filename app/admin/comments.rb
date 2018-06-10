968656921d6b88b983fa771db326a1dbf587841e
Kevin Kuipers <nanark@gmail.com>
2018-06-10 13:25:32 +0200

    AA Comments + handling active comments count only.

--- app/admin/comments.rb
+++ app/admin/comments.rb
@@ -1,11 +1,8 @@
 #
-ActiveAdmin.register Pew do
-  menu priority: 3
-
+ActiveAdmin.register Comment do
   permit_params :user_id,
                 :emotion_id,
                 :status,
-                :hashtag,
                 :sound
 
   filter :status, as: :select, collection: proc { Pew.statuses }
@@ -13,9 +10,9 @@ ActiveAdmin.register Pew do
   index do
     id_column
     column :hashtag do |item|
-      "##{item.hashtag}"
+      "##{item.pew.hashtag}"
     end
-    column :pew do |item|
+    column :comment do |item|
       div do
         span item.emotion.name
         span auto_link item, item.user.name
@@ -24,7 +21,6 @@ ActiveAdmin.register Pew do
     end
     column :likes, :likes_count
     column :plays, :plays_count
-    column :comments, :comments_count
     column :created_at do |item|
       time_ago(item.created_at)
     end
@@ -38,11 +34,16 @@ ActiveAdmin.register Pew do
     attributes_table do
       row :user
       row :emotion
-      row :hashtag do |item|
-        "##{item.hashtag}"
+      row :pew do |item|
+        auto_link item.pew, "##{item.pew.hashtag} from #{item.pew.user.name}"
       end
-      if pew.sound.attached?
-        row :pew do |item|
+      row :in_reply_to do |item|
+        if item.comment
+          auto_link item.comment, "@#{item.comment.user.name} ##{item.comment.id}"
+        end
+      end
+      if comment.sound.attached?
+        row :comment do |item|
           audio_tag(url_for(item.sound), controls: true)
         end
       end
@@ -50,19 +51,15 @@ ActiveAdmin.register Pew do
         status_tag item.status if item.status
       end
       row :likes_count
-      row :comments_count
       row :plays_count
       row :uuid
     end
-
-    render partial: 'comments', locals: { data: pew }
   end
 
   form do |f|
     f.inputs do
       f.input :sound, as: :file
       f.input :user_id, as: :select, collection: User.all.map { |i| [i.name, i.id] }, include_blank: false
-      f.input :hashtag
       f.input :emotion_id, as: :select, collection: Emotion.all.map { |i| [i.name, i.id] }, include_blank: false
       f.input :status, as: :select, collection: Pew.statuses.keys, include_blank: false
     end
