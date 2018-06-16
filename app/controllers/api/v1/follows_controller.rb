e9b047f64c2b67c7670ea74f669e7c948b8b4e71
Kevin Kuipers <nanark@gmail.com>
2018-06-16 18:25:06 +0200

    Follows

---
+++ app/controllers/api/v1/follows_controller.rb
@@ -0,0 +1,39 @@
+#
+class Api::V1::FollowsController < Api::V1::BaseController
+  before_action :fetch_user, only: %i[create delete]
+
+  def create
+    payload = {
+      user_id: current_user.id,
+      followed: @user
+    }
+    follows = Follower.first_or_create(payload)
+
+    api_error(status: 500, errors: follows.errors) and return false unless
+      follows.valid?
+
+    render json: { data: true }
+  end
+
+  def delete
+    payload = {
+      user_id: current_user.id,
+      followed: @user
+    }
+    follows = Follower.find_by(payload)
+
+    api_error(status: 500, errors: 'Like missing') and return false unless
+      follows.present?
+
+    follows.destroy
+
+    render json: { data: true }
+  end
+
+  private
+
+  def fetch_user
+    @user = User.where.not(id: current_user.id).find_by(uuid: params.fetch(:uuid))
+    api_error(status: 500, errors: 'User missing') and return false unless @user
+  end
+end
