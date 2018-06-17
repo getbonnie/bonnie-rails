60dbea539f5039a45203c563cedf518da30c9864
Kevin Kuipers <nanark@gmail.com>
2018-06-17 09:19:52 +0200

    Fix Duplication

--- app/controllers/api/v1/follows_controller.rb
+++ app/controllers/api/v1/follows_controller.rb
@@ -3,11 +3,7 @@ class Api::V1::FollowsController < Api::V1::BaseController
   before_action :fetch_user, only: %i[create delete]
 
   def create
-    payload = {
-      user_id: current_user.id,
-      following: @user
-    }
-    follows = Follower.first_or_create(payload)
+    follows = Follower.first_or_create(@payload)
 
     api_error(status: 500, errors: follows.errors) and return false unless
       follows.valid?
@@ -16,11 +12,7 @@ class Api::V1::FollowsController < Api::V1::BaseController
   end
 
   def delete
-    payload = {
-      user_id: current_user.id,
-      following: @user
-    }
-    follows = Follower.find_by(payload)
+    follows = Follower.find_by(@payload)
 
     api_error(status: 500, errors: 'Like missing') and return false unless
       follows.present?
@@ -33,7 +25,12 @@ class Api::V1::FollowsController < Api::V1::BaseController
   private
 
   def fetch_user
-    @user = User.where.not(id: current_user.id).find_by(uuid: params.fetch(:uuid))
+    user = User.where.not(id: current_user.id).find_by(uuid: params.fetch(:uuid))
     api_error(status: 500, errors: 'User missing') and return false unless @user
+
+    @payload = {
+      user_id: current_user.id,
+      following: user
+    }
   end
 end
--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -3,11 +3,7 @@ class Api::V1::LikesController < Api::V1::BaseController
   before_action :fetch_source, only: %i[create delete]
 
   def create
-    payload = {
-      user_id: current_user.id,
-      likable: @object
-    }
-    like = Like.first_or_create(payload)
+    like = Like.first_or_create(@payload)
 
     api_error(status: 500, errors: like.errors) and return false unless
       like.valid?
@@ -16,11 +12,7 @@ class Api::V1::LikesController < Api::V1::BaseController
   end
 
   def delete
-    payload = {
-      user_id: current_user.id,
-      likable: @object
-    }
-    like = Like.find_by(payload)
+    like = Like.find_by(@payload)
 
     api_error(status: 500, errors: 'Like missing') and return false unless
       like.present?
@@ -41,6 +33,12 @@ class Api::V1::LikesController < Api::V1::BaseController
               elsif params.fetch(:type) == 'comment'
                 fetch_comment
               end
+
+
+    @payload = {
+      user_id: current_user.id,
+      likable: @object
+    }
   end
 
   def fetch_pew
