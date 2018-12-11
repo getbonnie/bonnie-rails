fcec507b1ef0704bc93710a848b9094810a2e1c4
Kevin Kuipers <nanark@gmail.com>
2018-12-11 08:45:28 +0100

    fix test

--- spec/controllers/api/v1/comments_controller_spec.rb
+++ spec/controllers/api/v1/comments_controller_spec.rb
@@ -44,7 +44,7 @@ RSpec.describe Api::V1::CommentsController, type: :controller do
     expect(response.status).to eq(200)
 
     expect(pew.reload.comments_count).to eq(2)
-    expect(Notification.count).to eq(3)
+    expect(Notification.count).to eq(2)
   end
 
   it 'fails post with previous comment missing' do
