7f00a8e979b72556754f1ffad287928e54d4e83b
Kevin Kuipers <nanark@gmail.com>
2019-04-17 22:45:01 +0200

    Get contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,6 +3,7 @@ class Contact < ApplicationRecord
   before_save :default_values
 
   belongs_to :user
+  belongs_to :synced_user, class_name: 'User', primary_key: :phone_number, foreign_key: :phone, inverse_of: :synced_contacts, optional: true
 
   validates :phone_number, phone: true
 
--- spec/controllers/api/v1/contacts_controller_spec.rb
+++ spec/controllers/api/v1/contacts_controller_spec.rb
@@ -1,6 +1,22 @@
 require 'rails_helper'
 # !
 RSpec.describe Api::V1::ContactsController, type: :controller do
+  it 'fetch users' do
+
+    user = create(:user, phone: '+33600000001')
+    know_me = create(:user, phone: '+33600000002')
+    known = create(:user, phone: '+33600000003')
+
+    create(:contact, user: user, phone_number: known.phone)
+    create(:contact, user: know_me, phone_number: user.phone)
+
+    request.headers[:user] = user.id
+    get :index
+
+    expect(response.status).to eq(200)
+    expect(JSON.parse(response.body)['data'].length).to eq(2)
+  end
+
   it 'imports' do
 
     data = [{"name":"Kate Bell","phone_numbers":["(555) 564-8583","(415) 555-3695"]},{"name":"Daniel Higgins","phone_numbers":["555-478-7672","(408) 555-5270","(408) 555-3514"]},{"name":"John Appleseed","phone_numbers":["888-555-5512","888-555-1212"]},{"name":"Anna Haro","phone_numbers":["555-522-8243"]},{"name":"Hank M. Zakroff","phone_numbers":["(555) 766-4823","(707) 555-1854"]},{"name":"David Taylor","phone_numbers":["555-610-6679"]}]
