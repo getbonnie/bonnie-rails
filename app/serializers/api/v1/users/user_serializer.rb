c16370e997bd1af4be54305f1eedaf4e00f25032
Kevin Kuipers <nanark@gmail.com>
2019-05-30 11:53:07 +0200

    Fixes

--- app/serializers/api/v1/users/user_serializer.rb
+++ app/serializers/api/v1/users/user_serializer.rb
@@ -1,4 +1,12 @@
 #
 class Api::V1::Users::UserSerializer < Api::V1::Users::UserRootSerializer
   attributes :following_count, :followed_count
+
+  def following
+    if current_user
+      object.followed_by.where(followed_id: current_user.id).present?
+    else
+      false
+    end
+  end
 end
