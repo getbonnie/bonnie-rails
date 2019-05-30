ac5e908130bf906cf9a010289362e576b6882e09
Kevin Kuipers <nanark@gmail.com>
2019-05-30 12:11:02 +0200

    Fix 2

--- app/serializers/api/v1/users/user_serializer.rb
+++ app/serializers/api/v1/users/user_serializer.rb
@@ -4,7 +4,7 @@ class Api::V1::Users::UserSerializer < Api::V1::Users::UserRootSerializer
 
   attribute :following do
     if current_user
-      object.followed_by.where(followed_id: current_user.id).present?
+      object.followed_by.where(user: current_user.id).present?
     else
       false
     end
